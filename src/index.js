3bf43975163f9e5edf63a400865e3d002cc00b86
Chukwuma Ezumezu <aezumezu@gmail.com>
2017-03-08 14:03:50 +0100

    modify books path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, IndexRoute, browserHistory} from 'react-router';
+import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router';
 
 // import custom components
 import App from './components/App';
@@ -12,14 +12,16 @@ import BookDetail from './components/BookDetails';
 import './styles/styles.css';
 
 function redirectToHome() {
-  window.location.href = "/";
+  window.location.href = '/';
 }
-const router = <Router history={browserHistory}>
-  <Route path="/" component={App}>
-    <IndexRoute component={Home} />
-    <Route path="/book/:bookid" component={BookDetail} />
-    <Route path="*" onEnter={redirectToHome} />
-  </Route>
-</Router>
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="/books/:bookid" component={BookDetail} />
+      <Route path="*" onEnter={redirectToHome} />
+    </Route>
+  </Router>
+);
 
 ReactDOM.render(router, document.getElementById('root'));
