0f022bf8670a939ffa94881a1d3cf1ac739653d9
Chukwuma Ezumezu <aezumezu@gmail.com>
2017-03-08 12:18:04 +0100

    initial project scaffold

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'app/build/');
+const APP_DIR = path.resolve(__dirname, 'src/');
+const ENV = process.env.NODE_ENV || 'development';
+
+const config = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: 'build/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader?limit=100000"
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      },
+    ]
+  },
+  devtool: ENV === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
+
+  devServer: {
+    port: process.env.PORT || 8080,
+    host: 'localhost',
+    publicPath: '/build/',
+    contentBase: 'app/',
+    historyApiFallback: true,
+    open: true
+  }
+};
+
+module.exports = config;
