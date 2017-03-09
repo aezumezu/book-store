ef4a611ad17598acd4419d0ce9f6abf831f991be
Chukwuma Ezumezu <aezumezu@gmail.com>
2017-03-09 12:54:32 +0100

    add single bookview

--- src/components/BookDetails/BookView.js
+++ src/components/BookDetails/BookView.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Rating from '../Imports/ratings';
 
-const BookView = (props) => <div>
-  {console.log(props.bookInfo)}
+const BookView = (props) => <div className="book-detail">
+  <div>
+    <img src={`assets/book_covers/${props.bookInfo.thumbnail}`} />
+  </div>
+  <div className="book-info">
+    <span className="book-name">{props.bookInfo.title}</span>
+    <span className="book-author"> by {props.bookInfo.author}, <em>(Author)</em></span>
+    <span className="book-date">{props.bookInfo.datePublished}</span>
+    <span>
+      <Rating rating={props.bookInfo.rating} />
+    </span>
+    <span className="book-description">
+      {props.bookInfo.description
+        .split('<p>')
+        .map(para => <p>{para}</p>)
+      }
+    </span>
+  </div>
 </div>
 
 export default BookView;
